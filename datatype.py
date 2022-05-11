8d6791be5944829e8a79379b1120aaecb5320c71
Diego Guido <20203tn026@utez.edu.mx>
2022-05-11 18:06:50 -0500

    First Commit

---
+++ datatype.py
@@ -0,0 +1,43 @@
+
+#Strings
+print ("""Hello World""")
+print ('Hello World')
+
+# Intreger
+
+print(30)
+
+# Float
+
+print(30.1)
+
+# Boolean
+True
+False
+
+# List
+
+print(type([10, 20, 30, 55]))
+
+# Tuples
+
+(10, 20, 30)
+
+# Diccionarios
+
+dic = {
+ "age" : 3,
+ "name" : "Diego",
+ "last-name" : "Guido"
+}
+
+dic ['nickname'] = 'Deonx'
+dic ['nickname'] = 'Bido'
+
+print(dic)
+
+print(type({
+ "age" : 3,
+ "name" : "Diego",
+ "last-name": "Guido"
+}))
